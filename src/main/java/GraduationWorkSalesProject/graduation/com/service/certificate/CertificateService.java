0c78f70185835844f13cfd192abc68a7f2a20084
seonpilKim <ksp970306@gmail.com>
2022-01-21 07:02:10 +0900

    Feat: 불필요 파일 제거

--- src/main/java/GraduationWorkSalesProject/graduation/com/service/certificate/CertificateService.java
+++
@@ -1,9 +0,0 @@
-package GraduationWorkSalesProject.graduation.com.service.certificate;
-
-import GraduationWorkSalesProject.graduation.com.entity.certify.Certificate;
-
-public interface CertificateService {
-    void save(Certificate certificate);
-    void delete(String token);
-    void validateCertificate(String token);
-}
